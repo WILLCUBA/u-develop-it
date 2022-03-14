256361a5e6b2e362ad7d0f82988de702827f8e02
Will <wilito1210@gmail.com>
2022-03-13 23:27:16 -0400

    added votes table

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '93101231947*MySql',
+  password: '',
   database: 'election'
 });
 
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { route } = require('express/lib/application');
-const router = express.Router()
+const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));

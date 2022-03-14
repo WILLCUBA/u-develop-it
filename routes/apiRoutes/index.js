bb3ad6a741549672484dc38d5f082d81f20f7a6d
Will <wilito1210@gmail.com>
2022-03-13 23:12:00 -0400

    added voter routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { route } = require('express/lib/application');
+const router = express.Router()
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
+
+module.exports = router;
